aeed83a41768092f68ac38647d5c2c7e4f7ab107
8g2qrt4ksp-sys <8g2qrt4ksp@privaterelay.appleid.com>
2026-01-20 21:41:28 -0500

    Refactor service worker for cache versioning and activation
    Updated service worker to version 2 with cache management.

--- sw.js
+++ sw.js
@@ -1,8 +1,22 @@
-self.addEventListener("install",e=>{
- e.waitUntil(
-  caches.open("finanzas-v1").then(c=>c.addAll(["./","index.html"]))
- )
-})
-self.addEventListener("fetch",e=>{
- e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))
-})
+self.addEventListener("install", e => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open("finanzas-v2").then(c =>
+      c.addAll(["./","index.html","logo.png"])
+    )
+  );
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
